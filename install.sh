9f14e3f30f05e402584b05c8def3ddff33cafc55
刘子铭 <liuziming@liuzimingdeMac-mini.local>
2025-09-24 11:29:21 +0800

    fix install script

--- install.sh
+++ install.sh
@@ -2,22 +2,26 @@
 echo "are you going to install in this dictionary? (yes/no)"
 read t
 if [ $t == "yes" ]; then
-	curl -L -O "https://github.com/galoistom/web_for_server/blob/main/mainpack.tar.gz"
-	tar -czvf ./mainpack.tar.gz
+	curl -L -O https://raw.githubusercontent.com/galoistom/web_for_server/main/mainpack.tar.gz
+	tar -xzvf ./mainpack.tar.gz
 	cd ./mainpack/
 	echo "choose your operating system (1 for windows, 2 for mac, 3 for linux)"
 	read oper
 	if [ $oper == "1" ]; then
-		curl -L -O "https://github.com/galoistom/web_for_server/blob/main/build/web_for_server_win_x86.exe"
+		curl -L -O "https://raw.githubusercontent.com/galoistom/web_for_server/main/build/web_for_server_win_x86"
+		chmod +x ./web_for_server_win_x86.exe
 	elif [ $oper == "2" ]; then
-		curl -L -O "https://github.com/galoistom/web_for_server/blob/main/build/web_for_server_mac_arm"
+		curl -L -O "https://raw.githubusercontent.com/galoistom/web_for_server/main/build/web_for_server_mac_arm"
+		chmod +x ./web_for_server_mac_arm
 	elif [ $oper == "3" ]; then
 		echo "choose your chip (1 for x86, 2 for arm)"
 		read chip
 		if [ $chip == "1" ]; then
-			curl -L -O "https://github.com/galoistom/web_for_server/blob/main/build/web_for_server_linux_x86"
+			curl -L -O "https://github.com/galoistom/web_for_server/main/build/web_for_server_linux_x86"
+			chmod +x ./web_for_server_linux_x86
 		elif [ $chip == "2" ]; then
-			curl -L -O "https://github.com/galoistom/web_for_server/blob/main/build/web_for_server_linux_x86"
+			curl -L -O "https://github.com/galoistom/web_for_server/main/build/web_for_server_linux_arm"
+			chmod +x ./web_for_server_linux_arm
 		else
 			echo "input wrongly"
 			exit
@@ -26,8 +30,8 @@ if [ $t == "yes" ]; then
 		echo "input wrongly"
 		exit
 	fi
-	rm -rf ./mainpack.tar.gz
-	echo "download successfully"
+	rm -rf ../mainpack.tar.gz
+	echo "download successfully\nremember to change the server position into your own position in the config file"
 elif [ $t == "no" ]; then
 	echo "process stopped"
 else
